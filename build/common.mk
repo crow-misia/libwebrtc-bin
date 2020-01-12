ea69b00bc1a4bb215214b5776f0b5f1fff018a51
Zenichi Amano <crow.misia@gmail.com>
2020-01-12 20:00:20 +0900

    iOSに対応
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

--- build/common.mk
+++ build/common.mk
@@ -1,28 +1,38 @@
 BASE_DIR := $(CURDIR)/../..
 
+empty :=
+space:= $(empty) $(empty)
+
 ifeq ($(USE_CCACHE),1)
 CC_WRAPPER := cc_wrapper="$(THIRD_PARTY_DIR)/ccache"
 else
 CC_WRAPPER :=
 endif
 
-PACKAGE_SUFFIX :=
+PACKAGE_NAME := libwebrtc
+
+ifneq (x$(TARGET_OS),x)
+PACKAGE_NAME += -$(TARGET_OS)
+endif
+ifneq (x$(TARGET_CPU),x)
+PACKAGE_NAME += -$(TARGET_CPU)
+endif
 
 ifeq ($(USE_H264),1)
 RTC_USE_H264 := rtc_use_h264=true
-PACKAGE_SUFFIX += -h264
+PACKAGE_NAME += -h264
 else
 RTC_USE_H264 := rtc_use_h264=false
 endif
 
 ifeq ($(USE_X11),1)
 RTC_USE_X11 := rtc_use_x11=true
-PACKAGE_SUFFIX += -x11
+PACKAGE_NAME += -x11
 else
 RTC_USE_X11 := rtc_use_x11=false
 endif
 
-.PHONY: clean
+.PHONY: common-clean
 clean:
 	rm -rf $(PACKAGE_DIR)
 	rm -rf $(BUILD_DIR)
@@ -31,13 +41,13 @@ clean:
 download:
 	$(WEBRTC_DIR)/src/build/linux/sysroot_scripts/install-sysroot.py --arch=$(TARGET_CPU)
 
-.PHONY: package
-package: copy
+.PHONY: common-package
+common-package: copy
 	cd $(PACKAGE_DIR) && \
-	tar -Jcf libwebrtc-$(TARGET_OS)-$(TARGET_CPU)$(strip $(PACKAGE_SUFFIX)).tar.xz include lib NOTICE VERSION
+	tar -Jcf $(subst $(space),,$(PACKAGE_NAME)).tar.xz include lib NOTICE VERSION
 
-.PHONY: copy
-copy:
+.PHONY: common-copy
+common-copy:
 	rm -rf $(PACKAGE_DIR)
 	mkdir -p $(PACKAGE_DIR)/lib
 	mkdir -p $(PACKAGE_DIR)/include
