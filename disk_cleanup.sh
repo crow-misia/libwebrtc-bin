4c5bef8b141ea6de0daa6f239b8a53739412310c
Zenichi Amano <crow.misia@gmail.com>
2020-05-23 09:25:48 +0900

    GitHub Actionsの仕様変更による容量不足が発生していたため、設定を戻す
    https://github.com/actions/virtual-environments/issues/709
    
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

--- disk_cleanup.sh
+++ disk_cleanup.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# swapファイルの削除
-sudo swapoff -a
-sudo rm -f /swapfile
-
-# aptのキャッシュの削除
-sudo apt clean
-
 # Cache 済み Docker Image の削除
 docker rmi $(docker images -q -a)
-
-# Android SDK の削除
-sudo rm -rf ${ANDROID_HOME}
-
-# JVM の削除
-sudo rm -rf ${JAVA_HOME_12_X64}
-sudo rm -rf ${JAVA_HOME_11_X64}
-sudo rm -rf ${JAVA_HOME_8_X64}
-sudo rm -rf ${JAVA_HOME_7_X64} 
 
 # Boost の削除
 sudo rm -rf /usr/local/share/boost
