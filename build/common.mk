c7a61d6333ec150cf7f655ae62cdadce665f9e17
Zenichi Amano <crow.misia@gmail.com>
2026-03-31 16:07:11 +0900

    fix: nacl_armv6_2パッチ削除
    OPUS周りの変更によりパッチが不要となった
    
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

--- build/common.mk
+++ build/common.mk
@@ -47,7 +47,6 @@ endif
 common-patch:
 	echo "apply patches ..." && \
 	cd $(SRC_DIR) && \
-	patch -p1 < $(PATCH_DIR)/nacl_armv6_2.patch && \
 	patch -p1 < $(PATCH_DIR)/4k.patch && \
 	patch -p1 < $(PATCH_DIR)/add_licenses.patch && \
 	patch -p1 < $(PATCH_DIR)/add_deps.patch
